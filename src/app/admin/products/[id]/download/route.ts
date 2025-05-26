2feeb8527821e21aea6ca0ab1276f31de4a01210
Michael Okoronkwo <michaelokoronkwo59@gmail.com>
2025-05-26 13:07:43 +0100

    Done the rest of the promises

--- src/app/admin/products/[id]/download/route.ts
+++ src/app/admin/products/[id]/download/route.ts
@@ -2,9 +2,8 @@ import { prisma } from "@/db/db";
 import { NextRequest, NextResponse } from "next/server";
 import fs from "fs/promises";
 
-export async function GET(req: NextRequest,{params: {id} } : {params: {id: string}}){
-
-
+export async function GET(req: NextRequest,{params} : {params: Promise<{id: string}>}){
+    const {id} = await params;
     const product = await prisma.product.findUnique({ where: {id}, select: {filePath: true, name: true} })
     if(!product) return new Response("Product not found", {status: 404})
     
--- src/app/admin/products/[id]/edit/page.tsx
+++ src/app/admin/products/[id]/edit/page.tsx
@@ -3,11 +3,8 @@ import ProductForm from "../../_components/productForm";
 import { prisma } from "@/db/db";
 
 
-export default async function EditProductPage({
-    params : {id},
-    }: {
-    params : {id: string}
-}){
+export default async function EditProductPage({params}: {params : Promise<{id: string}>}){
+    const {id} = await params;
     const product = await prisma.product.findUnique({where: {id}})
     return <>
         <PageHeader>Edit Product</PageHeader>
